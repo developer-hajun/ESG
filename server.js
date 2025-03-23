421f0d83d5edfa49ba31b732db6364ba6bc1feaf
developer-hajun <juni8828@naver.com>
2025-03-23 18:07:17 +0900

    검색기능 추가 그런데 검색이후 마커 디자인 변경 이슈 발생

--- server.js
+++ server.js
@@ -1,8 +1,47 @@
 const express = require("express");
 const path = require("path");
+const { Pool } = require('pg');
 
 const app = express();
 const PORT = 3000;
+
+
+
+const pool = new Pool({
+  user: 'ihajun',
+  host: 'localhost',
+  database: 'postgres',
+  password: '1234',
+  port: 5432,
+});
+
+app.get('/api/companies', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT name, latitude, longitude FROM company');
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Server Error');
+  }
+});
+
+app.get('/api/find', async (req, res) => {
+  const searchTerm = req.query.q; // 쿼리 파라미터에서 검색어를 가져옵니다.
+
+  if (!searchTerm) {
+    return res.status(400).send('검색어를 입력해주세요.');
+  }
+  try {
+    const result = await pool.query(
+      'SELECT name, latitude, longitude FROM company WHERE name ILIKE $1',
+      [`%${searchTerm}%`] // 대소문자를 구분하지 않고 검색어가 포함된 이름을 찾습니다.
+    );
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('서버 오류');
+  }
+});
 
 // EJS 설정
 app.set("view engine", "ejs");
@@ -19,3 +58,4 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
     console.log(`서버 실행 중: http://localhost:${PORT}`);
 });
+
